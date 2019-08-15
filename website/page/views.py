fc19ccadec546dc73b61019bea68978d66383516
Bryan  Perez <Bryan@Bryans-MacBook-Pro.local>
2019-08-14 18:23:36 -0700

    Website-Bryan Perez

---
+++ website/page/views.py
@@ -0,0 +1,15 @@
+'''
+from django.shortcuts import render
+
+# Create your views here.
+#the bottom was from the video of boston
+from django.http import HttpResponse
+from django.template import loader
+def index(request):
+    template=loader.get_template('')
+    return HttpResponse("<h1> Hello World!!! <h1>")
+'''
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')

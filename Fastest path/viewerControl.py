fbdd42988078d0596d18c6b264a48d83a24adea2
Bryan <bryan0137@gmail.com>
2020-06-26 16:20:45 -0700

    blank game board
    array of game board created init to zero. Every zero in array creates a block on screen.

--- Fastest path/viewerControl.py
+++ Fastest path/viewerControl.py
@@ -3,9 +3,20 @@ import finder
 from graphics import *
 
 def main():
-    win = GraphWin("my window",500,500)
+    block_size=20
+    screen_width=500
+    screen_height=500
+    w, h = screen_width/block_size, screen_height/block_size
+    gameboard = [[0 for x in range(w)] for y in range(h)] 
+    win = GraphWin("Path Finder",screen_width,screen_height)
     win.setBackground('white')
-    
+    for i in range(len(gameboard)):
+        for j in range(len(gameboard[i])):
+            if gameboard[i][j]==0:
+                rec=Rectangle(Point(0+(block_size*j),0+(block_size*i)),Point(block_size+(block_size*j),block_size+(block_size*i)))
+                rec.draw(win)
+
     win.getMouse()
+
     win.close()
 main()

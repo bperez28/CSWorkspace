c4a7bc38aac6e799116bf5cacd1d70471fe9128c
Bryan <bryan0137@gmail.com>
2020-06-27 16:37:17 -0700

    added visuals on wall,start and end point
    -start is one click turns block green
    -end is two clicks turns block red
    -wall is three clicks turns block black

--- Fastest path/viewerControl.py
+++ Fastest path/viewerControl.py
@@ -12,19 +12,35 @@ def main():
     gameboard = [[0 for x in range(w)] for y in range(h)] 
     win = GraphWin("Path Finder",screen_width,screen_height)
     win.setBackground('white')
+
+    #game inti print 
     for i in range(len(gameboard)):
             for j in range(len(gameboard[i])):
                 if gameboard[i][j]==0:
                     rec=Rectangle(Point(0+(block_size*j),0+(block_size*i)),Point(block_size+(block_size*j),block_size+(block_size*i)))
                     rec.draw(win)
+                    
                 if gameboard[i][j]==1:
+                    # start printed
+                    rec=Rectangle(Point(0+(block_size*j),0+(block_size*i)),Point(block_size+(block_size*j),block_size+(block_size*i)))
+                    rec.setFill('green')
+                    rec.draw(win)
+                    
+                if gameboard[i][j]==2:
+                    #  end printed
+                    rec=Rectangle(Point(0+(block_size*j),0+(block_size*i)),Point(block_size+(block_size*j),block_size+(block_size*i)))
+                    rec.setFill('red')
+                    rec.draw(win)
+                if gameboard[i][j]==3:
+                    #  wall printed
                     rec=Rectangle(Point(0+(block_size*j),0+(block_size*i)),Point(block_size+(block_size*j),block_size+(block_size*i)))
                     rec.setFill('black')
                     rec.draw(win)
 
+
     # while loop to keep program running
     while win.isOpen():
-        print("runnning game")  
+        print("runnning game") 
         '''mouse clicks on the screen give back coordinates used
         for making walls,start and end point'''        
         coordinate = win.checkMouse()
@@ -37,10 +53,21 @@ def main():
         print(i)
         if coordinate != None:
             '''making wall'''
-            gameboard[i][j]=1
-            rec=Rectangle(Point(0+(block_size*j),0+(block_size*i)),Point(block_size+(block_size*j),block_size+(block_size*i)))
-            rec.setFill('black')
-            rec.draw(win)
+            if gameboard[i][j]>3:
+                gameboard[i][j]=0
+            gameboard[i][j]+=1
+            if gameboard[i][j]==1:
+                rec=Rectangle(Point(0+(block_size*j),0+(block_size*i)),Point(block_size+(block_size*j),block_size+(block_size*i)))
+                rec.setFill('green')
+                rec.draw(win)
+            if gameboard[i][j]==2:
+                rec=Rectangle(Point(0+(block_size*j),0+(block_size*i)),Point(block_size+(block_size*j),block_size+(block_size*i)))
+                rec.setFill('red')
+                rec.draw(win)
+            if gameboard[i][j]==3:
+                rec=Rectangle(Point(0+(block_size*j),0+(block_size*i)),Point(block_size+(block_size*j),block_size+(block_size*i)))
+                rec.setFill('black')
+                rec.draw(win)
         
         
 
